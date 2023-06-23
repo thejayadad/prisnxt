37f2265558451bfd4530b1a271cf88c7720aa13f
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-23 10:56:21 -0400

    udate

--- crud/app/page.tsx
+++ crud/app/page.tsx
@@ -1,9 +1,43 @@
-import Image from 'next/image'
+'use client'
+import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { prisma } from '../lib/prisma'
+
+interface Notes{
+  notes: {
+    id: string
+    title: string
+    content: string
+  }[]
+}
+
+interface FormData {
+  title: string
+  content: string
+  id: string
+}
 
 export default function Home() {
+  const [form, setForm] = useState<FormData>({title: '', content: '', id: ''})
   return (
     <main>
       <h2>Hello World</h2>
+      <form
+      onSubmit={e => {
+        e.preventDefault()
+      }}
+      >
+        <input
+        placeholder='Title'
+        />
+          <textarea
+        placeholder='Content'
+        
+        />
+        <button>Submit</button>
+
+      </form>
     </main>
   )
 }
