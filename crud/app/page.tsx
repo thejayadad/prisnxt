46a35b6040ca988a008afc83ae6fc436cd26d974
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-23 12:47:37 -0400

    update

--- crud/app/page.tsx
+++ crud/app/page.tsx
@@ -21,41 +21,9 @@ interface FormData {
 export default function Home() {
   const [form, setForm] = useState<FormData>({title: '', content: '', id: ''})
   return (
-    <>
-    <h2
-    className='text-center mt-10'
-    >Hello World</h2>
-    <main
-    className='h-100 w-full flex items-center justify-center'
-    >
-      <div
-      className='bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg'
-      >
-      <form
-      onSubmit={e => {
-        e.preventDefault()
-      }}
-      className='flex'
-      >
-        <div>
-        <input
-        placeholder='Title'
-        className='border-bottom rounded-3xl w-full py-2 px-3 mr-4 text-grey-darker'
-        />
-          <textarea
-        placeholder='Content'
-        className='border-bottom rounded-3xl w-full py-2 px-3 mr-4 text-grey-darker'
-        
-        />
-        </div>
-        <button
-        type='submit'
-        className='p-1 border-2 rounded-3xl'
-        >Submit</button>
+    <main>
+      <h2 className='text-5xl'>To Do App</h2>
 
-      </form>
-      </div>
     </main>
-    </>
-  )
+  ) 
 }
