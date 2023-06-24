053a8f02246415ad1d74db867945af1352826b82
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-23 21:16:14 -0400

    update

--- crud/app/page.tsx
+++ crud/app/page.tsx
@@ -22,41 +22,41 @@ export default function Home() {
   const [form, setForm] = useState<FormData>({title: '', content: '', id: ''})
   return (
     <main>
-      <h2 className='text-5xl'>To Do App</h2>
+      <h2 className='text-5xl text-center mt-10 text-white'>To Do App</h2>
     
     <div>
     <form className="w-full max-w-md mx-auto px-4 py-2">
         <div className="flex  items-center py-2 gap-3 align">
         <div className='flex flex-col gap-3 w-full max-w-md'>
         <input
-                className="appearance-none bg-transparent border-b-2 border-teal-500 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                className="bg-transparent border-b-2 border-orange-500 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none cursur-pointer"
                 type="text" placeholder="Title" />
             <input
-                className="bg-transparent border-b-2 border-teal-500 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+                className="bg-transparent border-b-2 border-orange-500 w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none cursor-pointer"
                 type="text" placeholder="Content" />
         </div>
             <button
-                className="bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-6 px-2 rounded"
+                className="bg-orange-500 hover:bg-orange-700 hover:border-orange-900 text-sm border-1 text-white py-8 px-2 rounded"
                 type="button">
                 Add
             </button>
         </div>
     </form>
 
-    <ul className="rounded-lg shadow max-w-md mx-auto flex flex-col">
-    <li className="px-6 py-4 rounded-lg bg-gray-100 mt-4 mb-8">
+    <ul className="rounded-lg shadow max-w-md mx-auto flex flex-col cursor-pointer">
+    <li className="px-6 py-4 rounded-lg bg-gray-100 mt-4 mb-8 hover:bg-gray-500">
         <div className="flex justify-between">
             <span className="font-semibold text-lg">List Item 1</span>
             <span className="text-gray-500 text-xs">1 day ago</span>
         </div>
         <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris.</p>
         <div className="flex justify-around mt-2">
-        <span className="text-xs "><button
-        className='border-2 bg-green-500 border-green-900 text-white hover:bg-green-300 p-2 ml-2 border-2 rounded'
+        <span className="text-xs"><button
+        className='border-2 bg-green-500 border-green-500 text-white hover:bg-green-700 p-4 ml-2 rounded'
         >Edit</button></span>
-            <span className="text-gray-500 text-xs">
+            <span className="text-xs">
               <button
-              className='bg-red-500 border-2 border-red-900 text-white hover:bg-red-300 p-2 ml-2 border-2 rounded'
+              className='border-2 bg-red-500 border-red-500 text-white hover:bg-red-700 p-4 ml-2 rounded'
               >Delete</button>
             </span>
         </div>
